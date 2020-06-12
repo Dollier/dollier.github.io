051144f2a5bdd1add1e192636463f68da70bae65
Dollier <d.co@tutanota.com>
2020-06-12 22:04:08 +0200

    starting landing pages

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>Not found </h1>
+    </Layout>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,53 @@
 import React from "react"
-import Layout from "../components/Layout"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { useInView } from "react-intersection-observer"
+import FadeInSection from "../utils/FadeinSection"
 
+let inViewTrigger = 0
 export default function Home() {
+  const [ref, inView, entry] = useInView({
+    /* Optional options */
+    threshold: 0,
+    rootMargin: "100px",
+  })
+  if (inView) {
+    inViewTrigger++
+  }
   return (
-    <Layout>
-      <h1>Home</h1>
-    </Layout>
+    <div>
+      <span ref={ref}></span>
+      <section className="landing">
+        <div
+          className={"hero-text"}
+          style={
+            !inView && inViewTrigger > 0
+              ? { animation: "logoFade 0.3s both" }
+              : inViewTrigger > 1
+              ? { animation: "opacityTitle 1s both" }
+              : { animation: "opacityTitle 1s 1.6s both" }
+          }
+        >
+          {!inView && inViewTrigger > 0}
+          Dorian Collier
+          <div>Fullstack Developer</div>
+          <div className="social-media">
+            <FontAwesomeIcon icon={faGithub} size="2x" color="#17171d" />
+            <FontAwesomeIcon icon={faGithub} size="2x" color="#17171d" />
+          </div>
+        </div>
+        <div className="productBg">
+          <div className="hero-content"></div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <FadeInSection>
+          <div className="projects-title">Projects</div>
+        </FadeInSection>
+      </section>
+      {/* <section className="cv">CV</section>
+      <section className="blog">Blog</section> */}
+    </div>
   )
 }
